7eb2227edf531b750963b3d329a739b367b3e0fd
elitech-official <completelydifferentuser@gmail.com>
2016-01-06 20:32:08 +0200

    styles and small fixes

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,11 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   
   resources :events
-  get 'contact', to: 'messages#new', as: :contact
+  get 'contact', to: 'messages#new', as: 'contact'
   post 'contact', to: 'messages#create'
   root to: 'events#title'
   get '/', to: 'events#title'
   get '/about-us', to: 'events#about_us', as: :about
   get '/team', to: 'events#team', as: :team
   get '/contacts', to: 'events#contacts', as: :contacts
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106135725) do
+ActiveRecord::Schema.define(version: 20160106181255) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20160106135725) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.string   "slug",        limit: 191
+    t.datetime "date"
   end
 
   create_table "messages", force: :cascade do |t|
